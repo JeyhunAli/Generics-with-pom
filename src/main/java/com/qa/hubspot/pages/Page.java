214d94b7f83fcfd6d6b4bf9f75b62120b12a08da
Jeyhun Ali <Ali-ceyhun@list.ru>
2019-10-09 18:02:06 -0400

    added text file and some more important note about using java jenerics with hybrid frame work and page object model

--- src/main/java/com/qa/hubspot/pages/Page.java
+++ src/main/java/com/qa/hubspot/pages/Page.java
@@ -6,16 +6,20 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Page {
 
 	public WebDriver driver;
-	public WebDriverWait wait;
+	public WebDriverWait wait;   // for explicitly wait 
 
-	// page class constructor:
+	// page class constructor:   to initialize the browers 
 	public Page(WebDriver driver, WebDriverWait wait) {
 		this.driver = driver;
 		this.wait = wait;
 	}
 
-	// create a method with Java Generics and return a new page
-	public <TPage extends BasePage> TPage getInstance(Class<TPage> pageClass) {
+	// create a method with Java Generics and return a new page  
+	//
+	public <TPage extends BasePage> TPage getInstance(Class<TPage> pageClass) { //here passing page class as parameters 
+		               //Tpage is definition of the generics   
+		             //here i created a method name getInstance and return type of this methos is Tpage 
+		             // and extends basepage
 		try {
 			return pageClass.getDeclaredConstructor(WebDriver.class, WebDriverWait.class).newInstance(this.driver, this.wait);
 		} catch (Exception e) {
